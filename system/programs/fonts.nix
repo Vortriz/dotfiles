c0b5bc594c856b9a4109f7f377423359ae9d8038
Vortriz <vorarishi22+github@gmail.com>
2024-12-17 12:29:22 +0530

    feat(fonts): add roboto, source sans pro and fontawesome

--- system/programs/fonts.nix
+++ system/programs/fonts.nix
@@ -5,6 +5,9 @@
     fonts.packages = with pkgs; [
         HelveticaNeueCyr
         SFMono
+        font-awesome
+        roboto
+        source-sans-pro
         nerd-fonts.fira-code
         nerd-fonts.jetbrains-mono
         (google-fonts.override { fonts = [ "Inter" "Overpass" "Rubik" ]; })
