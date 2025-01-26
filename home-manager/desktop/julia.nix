68895a241ad02880705a9d9ebd28d2823d87b0da
Vortriz <vorarishi22+github@gmail.com>
2025-01-26 11:43:45 +0530

    feat(julia): auto-activate project

--- home-manager/desktop/julia.nix
+++ home-manager/desktop/julia.nix
@@ -9,4 +9,14 @@
         ];
         enableNVIDIA = false;
     };
+
+    home.file = {
+        ".julia/config/startup.jl".text = ''
+            using Pkg
+
+            if isfile("Project.toml") && isfile("Manifest.toml")
+                Pkg.activate(".")
+            end
+        '';
+    };
 }
