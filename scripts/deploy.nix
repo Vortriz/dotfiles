051f131ec6b2db9cad1ed811c23a1d38bb572580
Vortriz <vorarishi22+github@gmail.com>
2025-02-13 15:10:48 +0530

    tweak(deploy): can now be executed from any dir

--- scripts/deploy.nix
+++ scripts/deploy.nix
@@ -7,12 +7,14 @@
             echo "Rebuilding new generation..."
             nh os switch
             if test $status = 0
+                cd $FLAKE
                 set gen $(nixos-rebuild list-generations --flake $FLAKE | grep -oP "[0-9]*(?= current)")
                 set timestamp $(date '+%x %X')
                 set branch $(git branch --show-current)
                 echo "$gen - $timestamp" >> $FLAKE/build.log
                 git add -A
                 git commit -m "deployed $gen through $branch"
+                cd -
             else
                 echo "Failed to deploy!"
             end
