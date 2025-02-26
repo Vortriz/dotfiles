21474b1aaf0dd2e40d291098685363cc91dee801
Vortriz <vorarishi22+github@gmail.com>
2025-02-27 00:16:04 +0530

    fix(vscode): syntax deprecation

--- home/software/programming/vscode.nix
+++ home/software/programming/vscode.nix
@@ -5,98 +5,100 @@
     programs.vscode = {
         enable = true;
 
-        userSettings = {
-            # use FiraCode (to fix broken icons)
-            "terminal.integrated.fontFamily" = "'FiraCode Nerd Font Mono'";
+        profiles.default = {
+            userSettings = {
+                # use FiraCode (to fix broken icons)
+                "terminal.integrated.fontFamily" = "'FiraCode Nerd Font Mono'";
 
-            # because this is the only sane thing
-            "editor.detectIndentation" = false;
-            "editor.tabSize" = 4;
-            "[nix]" = {
+                # because this is the only sane thing
                 "editor.detectIndentation" = false;
                 "editor.tabSize" = 4;
+                "[nix]" = {
+                    "editor.detectIndentation" = false;
+                    "editor.tabSize" = 4;
+                };
+
+                # clean files
+                "files.trimTrailingWhitespace" = true;
+
+                # better diffs
+                "diffEditor.ignoreTrimWhitespace" = false;
+
+                # cleaner file explorer
+                "workbench.tree.indent" = 12;
+
+                # zoom slightly
+                "window.zoomLevel" = 1.2;
+
+                # annoying confimation
+                "explorer.confirmDragAndDrop" = false;
+                "git.confirmSync" = false;
+
+                # nix LSP
+                "nix.enableLanguageServer" = true;
+                "nix.serverPath" = "nixd";
+
+                # fix annoying warning
+                "workbench.colorTheme" = "Stylix";
+
+                # julia
+                "julia.symbolCacheDownload" = true;
+                "terminal.integrated.commandsToSkipShell" = [
+                "language-julia.interrupt"
+                ];
+                "julia.enableTelemetry" = true;
+
+
+                # typst
+                "[typst]" = {
+                    "editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
+                };
+
+                # copilot
+                "github.copilot.enable" = {
+                    "shellscript" = false;
+                };
             };
 
-            # clean files
-            "files.trimTrailingWhitespace" = true;
+            extensions = with pkgs.vscode-extensions; [
+                # language packs et al
+                james-yu.latex-workshop
+                jnoortheen.nix-ide
+                julialang.language-julia
+                mikestead.dotenv
+                mkhl.direnv
+                ms-python.isort
+                ms-python.python
+                ms-python.vscode-pylance
+                ms-toolsai.jupyter
+                ms-toolsai.jupyter-renderers
+                ms-toolsai.vscode-jupyter-cell-tags
+                ms-toolsai.vscode-jupyter-slideshow
+                myriad-dreamin.tinymist
+                shd101wyy.markdown-preview-enhanced
+                yzhang.markdown-all-in-one
 
-            # better diffs
-            "diffEditor.ignoreTrimWhitespace" = false;
+                # themes et al
+                catppuccin.catppuccin-vsc
+                catppuccin.catppuccin-vsc-icons
+                github.github-vscode-theme
+                pkief.material-icon-theme
+                zhuangtongfa.material-theme
 
-            # cleaner file explorer
-            "workbench.tree.indent" = 12;
-
-            # zoom slightly
-            "window.zoomLevel" = 1.2;
-
-            # annoying confimation
-            "explorer.confirmDragAndDrop" = false;
-            "git.confirmSync" = false;
-
-            # nix LSP
-            "nix.enableLanguageServer" = true;
-            "nix.serverPath" = "nixd";
-
-            # fix annoying warning
-            "workbench.colorTheme" = "Stylix";
-
-            # julia
-            "julia.symbolCacheDownload" = true;
-            "terminal.integrated.commandsToSkipShell" = [
-               "language-julia.interrupt"
+                # misc
+                antfu.slidev
+                donjayamanne.githistory
+                github.copilot
+                github.copilot-chat
+                jock.svg
+                ms-azuretools.vscode-docker
+                ms-vscode-remote.remote-containers
+                ms-vsliveshare.vsliveshare
+                ritwickdey.liveserver
+                rubymaniac.vscode-paste-and-indent
+                tomoki1207.pdf
             ];
-            "julia.enableTelemetry" = true;
-
-
-            # typst
-            "[typst]" = {
-                "editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
-            };
-
-            # copilot
-            "github.copilot.enable" = {
-                "shellscript" = false;
-            };
         };
-
-        extensions = with pkgs.vscode-extensions; [
-            # language packs et al
-            james-yu.latex-workshop
-            jnoortheen.nix-ide
-            julialang.language-julia
-            mikestead.dotenv
-            mkhl.direnv
-            ms-python.isort
-            ms-python.python
-            ms-python.vscode-pylance
-            ms-toolsai.jupyter
-            ms-toolsai.jupyter-renderers
-            ms-toolsai.vscode-jupyter-cell-tags
-            ms-toolsai.vscode-jupyter-slideshow
-            myriad-dreamin.tinymist
-            shd101wyy.markdown-preview-enhanced
-            yzhang.markdown-all-in-one
-
-            # themes et al
-            catppuccin.catppuccin-vsc
-            catppuccin.catppuccin-vsc-icons
-            github.github-vscode-theme
-            pkief.material-icon-theme
-            zhuangtongfa.material-theme
-
-            # misc
-            antfu.slidev
-            donjayamanne.githistory
-            github.copilot
-            github.copilot-chat
-            jock.svg
-            ms-azuretools.vscode-docker
-            ms-vscode-remote.remote-containers
-            ms-vsliveshare.vsliveshare
-            ritwickdey.liveserver
-            rubymaniac.vscode-paste-and-indent
-            tomoki1207.pdf
-        ];
     };
 
     home.file.".vscode/argv.json".text = builtins.toJSON {
