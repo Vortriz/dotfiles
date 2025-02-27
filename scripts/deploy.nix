37a0acf38207e015ce664458e0ab893cf37e4f8d
Vortriz <vorarishi22+github@gmail.com>
2025-02-27 11:07:00 +0530

    feat(deploy): add diff to build-log using nvd

--- scripts/deploy.nix
+++ scripts/deploy.nix
@@ -8,12 +8,17 @@
             nh os switch
             if test $status = 0
                 cd $FLAKE
+
                 set gen $(nixos-rebuild list-generations --flake $FLAKE | grep -oP "[0-9]*(?= current)")
+                set prev_gen (math $gen - 1)
                 set timestamp $(date '+%x %X')
                 set branch $(git branch --show-current)
-                echo "$gen - $timestamp" >> $FLAKE/build.log
+
+                echo -e "\n---\nGen $gen - $timestamp" >> $FLAKE/build-log.md
+                nvd diff /nix/var/nix/profiles/system-{$prev_gen,$gen}-link >> $FLAKE/build-log.md
+
                 git add -A
-                git commit -m "deployed $gen through $branch"
+                git commit -m "deployed $gen via $branch"
                 cd -
             else
                 echo "Failed to deploy!"
