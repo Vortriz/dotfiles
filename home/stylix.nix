1f95e74017c0f81a74befae94c82dfbecb520d65
Rishi Vora <vorarishi22@gmail.com>
2025-03-03 15:02:01 +0530

    fix(stylix): oopsie

--- home/stylix.nix
+++ home/stylix.nix
@@ -6,6 +6,8 @@
         targets = {
             gtk.enable = true;
             kde.enable = false; # doesnt work for qt6 for now
+            bat.enable = true;
+            fzf.enable = true;
         };
     };
 }
