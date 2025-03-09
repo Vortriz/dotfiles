143019c78bc2001257f496c689a8a6ddd9e93c61
Vortriz <vorarishi22+github@gmail.com>
2025-03-09 16:04:19 +0530

    tweak(aria2): optimize downloads

--- home/terminal/software/aria2.nix
+++ home/terminal/software/aria2.nix
@@ -4,6 +4,9 @@
 
         settings = {
             dir = "/home/vortriz/downloads";
+            max-concurrent-downloads = 4;
+            split = 8;
+            max-connection-per-server = 8;
         };
     };
 }
