48457ec2dd0ac94fc33ebea495b7eeaeb83c0298
Rishi Vora <vorarishi22@gmail.com>
2025-03-12 02:16:48 +0530

    tweak(fish): move utilities, plugins and settings over to home-manager

--- home/terminal/shell/fish.nix
+++ home/terminal/shell/fish.nix
@@ -1,4 +1,7 @@
 {
+    pkgs,
+    ...
+}: {
     programs.fish = {
         enable = true;
 
@@ -14,6 +17,24 @@
         } //
         builtins.mapAttrs ( alias: x: "open $(ff${x})" ) { "ofp" = "p"; "ofs" = "s"; "ofv" = "v"; } //
         builtins.mapAttrs ( alias: x: "cd $(fd${x})" ) { "cdp" = "p"; "cds" = "s"; };
+    };
+
+    home.packages = with pkgs.fishPlugins; [
+        autopair
+        colored-man-pages
+        fish-bd
+        fzf-fish
+        puffer
+        sponge
+    ] ++ (with pkgs; [
+        fd
+        fzf
+        bat
+    ]);
+
+    home.sessionVariables = {
+        fzf_preview_dir_cmd = "eza -1 --all";
+        sponge_allow_previously_successful = "false";
     };
 
     stylix.targets.fish.enable = true;
--- system/programs/shells/fish.nix
+++ system/programs/shells/fish.nix
@@ -1,19 +1,5 @@
 {
-    pkgs,
-    ...
-}: {
     programs.fish = {
         enable = true;
-
-        shellInit = ''
-            direnv hook fish | source
-        '';
     };
-
-    environment.systemPackages = with pkgs; [
-        fishPlugins.colored-man-pages
-        fishPlugins.fzf-fish
-        fishPlugins.sponge
-        fishPlugins.z
-    ];
 }
