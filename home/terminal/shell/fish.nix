c93b3b8de6a371fcd38f0732150f4b77ea4d8cdc
Rishi Vora <vorarishi22@gmail.com>
2025-03-13 04:02:35 +0530

    tweak(fish): remove useless aliases

--- home/terminal/shell/fish.nix
+++ home/terminal/shell/fish.nix
@@ -4,19 +4,6 @@
 }: {
     programs.fish = {
         enable = true;
-
-        shellAliases =
-        let
-            ff = ( path: type: nth:  "fd . '/mnt/${path}' -t ${type} | fzf --delimiter='/' --with-nth ${nth}.. --wrap" );
-        in {
-            "ffv" = ff "SECONDARY/nonlinear vault" "f" "6";
-            "ffp" = ff "PRIMARY" "f" "5";
-            "ffs" = ff "SECONDARY" "f" "5";
-            "fdp" = ff "PRIMARY" "d" "5";
-            "fds" = ff "SECONDARY" "d" "5";
-        } //
-        builtins.mapAttrs ( alias: x: "open $(ff${x})" ) { "ofp" = "p"; "ofs" = "s"; "ofv" = "v"; } //
-        builtins.mapAttrs ( alias: x: "cd $(fd${x})" ) { "cdp" = "p"; "cds" = "s"; };
     };
 
     home.packages = with pkgs.fishPlugins; [
