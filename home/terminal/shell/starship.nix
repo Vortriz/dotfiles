6ddf46237e12b083dee6888c06fcb7bce5894946
Vortriz <vorarishi22+github@gmail.com>
2025-03-13 04:10:13 +0530

    chore: update inputs

--- home/terminal/shell/starship.nix
+++ home/terminal/shell/starship.nix
@@ -6,7 +6,7 @@
             readFile (
                 fetchurl {
                     url = "https://starship.rs/presets/toml/nerd-font-symbols.toml";
-                    sha256 = "sha256:1bk2bb4nfwppg0gz4yyid4prdcf5r7yis0h4x10l8blvzlzm452g";
+                    sha256 = "1bk2bb4nfwppg0gz4yyid4prdcf5r7yis0h4x10l8blvzlzm452g";
                 }
             )
         )) // {};
