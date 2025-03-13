5d1a500e9aab2a2525ef0dc28b381053cd80c9e2
Rishi Vora <vorarishi22@gmail.com>
2025-03-14 03:18:43 +0530

    feat(np): add nix prefetch script to get hash

--- scripts/default.nix
+++ scripts/default.nix
@@ -3,6 +3,7 @@
         ./battery.nix
         ./deploy.nix
         ./dl.nix
+        ./np.nix
         ./oimg.nix
         ./update.nix
     ];
--- scripts/oimg.nix
+++ scripts/oimg.nix
@@ -2,10 +2,14 @@
     pkgs,
     ...
 }: let
+    flameshot = "${pkgs.flameshot}/bin/flameshot";
+    magick = "${pkgs.imagemagick}/bin/magick";
+    wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+
     oimg-script = pkgs.writers.writeFishBin "oimg" {} ''
-        ${pkgs.flameshot}/bin/flameshot gui -p /tmp/img.png
-        ${pkgs.imagemagick}/bin/magick /tmp/img.png -negate -fuzz 20% -transparent black /tmp/img-processed.png && rm /tmp/img.png
-        ${pkgs.wl-clipboard}/bin/wl-copy < /tmp/img-processed.png && rm /tmp/img-processed.png
+        ${flameshot} gui -p /tmp/img.png
+        ${magick} /tmp/img.png -negate -fuzz 20% -transparent black /tmp/img-processed.png && rm /tmp/img.png
+        ${wl-copy} < /tmp/img-processed.png && rm /tmp/img-processed.png
     '';
 in {
     home.packages = [
