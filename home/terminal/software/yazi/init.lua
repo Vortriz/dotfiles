82272668db82066b575c4a5d27a16a84aef684fa
Rishi Vora <vorarishi22@gmail.com>
2025-03-16 02:39:34 +0530

    tweak(yazi): replace augment-command with file-navigation-wraparound

--- home/terminal/software/yazi/init.lua
+++ home/terminal/software/yazi/init.lua
@@ -18,30 +18,6 @@ require("yamb"):setup {
                 (os.getenv("HOME") .. "/.config/yazi/bookmark"),
 }
 
--- augment-command
-require("augment-command"):setup({
-    prompt = false,
-    default_item_group_for_prompt = "hovered",
-    smart_enter = false,
-    smart_paste = false,
-    smart_tab_create = false,
-    smart_tab_switch = false,
-    confirm_on_quit = false,
-    open_file_after_creation = false,
-    enter_directory_after_creation = false,
-    use_default_create_behaviour = false,
-    enter_archives = false,
-    extract_retries = 3,
-    recursively_extract_archives = false,
-    preserve_file_permissions = false,
-    must_have_hovered_item = false,
-    skip_single_subdirectory_on_enter = false,
-    skip_single_subdirectory_on_leave = false,
-    smooth_scrolling = false,
-    scroll_delay = 0.02,
-    wraparound_file_navigation = true,
-})
-
 -- custom-shell
 require("custom-shell"):setup({
     history_path = "default",
