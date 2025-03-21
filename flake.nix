cc11f7447529f59c7d4098ef1366b5f1530ac501
Vortriz <vorarishi22+github@gmail.com>
2025-03-21 16:40:15 +0530

    tweak(flake): simplify

--- flake.nix
+++ flake.nix
@@ -37,26 +37,25 @@
         ...
     } @ inputs: let
         inherit (self) outputs;
-        # Supported systems for your flake packages, shell, etc.
-        systems = [
-            "x86_64-linux"
-        ];
-        # This is a function that generates an attribute by calling a function you
-        # pass to it, with each system as an argument
-        forAllSystems = nixpkgs.lib.genAttrs systems;
+
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
     in {
         # Your custom packages
         # Accessible through 'nix build', 'nix shell', etc
-        packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+        packages = import ./pkgs pkgs;
+
         # Formatter for your nix files, available through 'nix fmt'
         # Other options beside 'alejandra' include 'nixpkgs-fmt'
-        formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+        formatter = pkgs.alejandra;
 
         # Your custom packages and modifications, exported as overlays
         overlays = import ./overlays {inherit inputs;};
+
         # Reusable nixos modules you might want to export
         # These are usually stuff you would upstream into nixpkgs
         nixosModules = import ./modules/nixos;
+
         # Reusable home-manager modules you might want to export
         # These are usually stuff you would upstream into home-manager
         homeManagerModules = import ./modules/home-manager;
