df967050cc8ad4e7e2daa918f934fa5b36e1a127
Rishi Vora <vorarishi22@gmail.com>
2025-03-25 02:10:12 +0530

    feat(typst-presentation): add template

---
+++ templates/typst-presentation/flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "Typst presentation environment Flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = {
+        nixpkgs,
+        ...
+    }: let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+        devShells.x86_64-linux.default = pkgs.mkShell {
+            packages = with pkgs; [
+                typst
+            ];
+        };
+    };
+}
