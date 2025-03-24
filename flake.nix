8cf896bf71ec28e294985a6ad5517c303c14c16d
Vortriz <vorarishi22+github@gmail.com>
2025-03-25 02:10:12 +0530

    feat(typst-presentation): add template

--- flake.nix
+++ flake.nix
@@ -66,6 +66,11 @@
                 path = ./templates/uv2nix;
                 description = "uv2nix Flake";
             };
+
+            typst-presentation = {
+                path = ./templates/typst-presentation;
+                description = "Typst presentation using custom Touying theme";
+            };
         };
 
         # NixOS configuration entrypoint
