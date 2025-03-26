c7ce42dd554a03f470abd7805ee8b7026ad85388
Rishi Vora <vorarishi22@gmail.com>
2025-03-26 23:20:06 +0530

    refactor: remove x11 and plasma6

--- system/services/default.nix
+++ system/services/default.nix
@@ -5,12 +5,10 @@
         ./keyring.nix
         ./logind.nix
         ./pipewire.nix
-        ./plasma6.nix
         ./preload.nix
         ./printing.nix
         ./sound.nix
         ./ssh.nix
         ./udisks2.nix
-        ./x11.nix
     ];
 }
