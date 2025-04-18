bf3d862830ac1f7833f978c0501f7b101ebfbf88
Rishi Vora <vorarishi22@gmail.com>
2025-04-18 23:24:52 +0530

    tweak(agenix): rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-    user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFtvfDuEyf7KsXDqDVxogGmHoBCtsFDVxmkPtnphZKWH root";
+    user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8LhZFvc5rrYsyhRs6knNTAPnW0+7/JKCXeuqNgSjKY root";
 in {
     "rclone.age".publicKeys = [user];
 }
