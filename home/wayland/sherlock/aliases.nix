77a2470435e2c071300644b8be9a4d8876c82aad
Rishi Vora <vorarishi22@gmail.com>
2025-04-20 23:33:01 +0530

    tweak(sherlock): split

--- home/wayland/sherlock/aliases.nix
+++ home/wayland/sherlock/aliases.nix
@@ -1,18 +1,16 @@
 {
-    programs.sherlock.settings.aliases = {
-        Kitty = {
-            name = "Kitty";
-            icon = "terminal";
-            keywords = "terminal";
-        };
-        VSCode = {
-            name = "Visual Studio Code";
-            icon = "code";
-            keywords = "code";
-        };
-        Sioyek = {
-            name = "Sioyek";
-            keywords = "sioyek";
-        };
+    Kitty = {
+        name = "Kitty";
+        icon = "terminal";
+        keywords = "terminal";
+    };
+    VSCode = {
+        name = "Visual Studio Code";
+        icon = "code";
+        keywords = "code";
+    };
+    Sioyek = {
+        name = "Sioyek";
+        keywords = "sioyek";
     };
 }
--- home/wayland/sherlock/launchers.nix
+++ home/wayland/sherlock/launchers.nix
@@ -1,77 +1,75 @@
-{
-    programs.sherlock.settings.launchers = [
-        {
-            name = "Clipboard";
-            type = "clipboard-execution";
-            args = {
-                capabilities = ["url" "colors.all"];
-            };
-            priority = 1;
-            home = true;
-        }
-        {
-            name = "Calculator";
-            type = "calculation";
-            args = {
-                capabilities = ["calc.math" "calc.units"];
-            };
-            priority = 1;
-        }
-        {
-            name = "App Launcher";
-            alias = "app";
-            type = "app_launcher";
-            args = {};
-            priority = 1;
-            home = true;
-        }
-        {
-            name = "Cloudflare Warp";
-            alias = "w";
-            type = "command";
-            args = {
-                commands = {
-                    "Connect Warp" = {
-                        exec = "warp-cli connect";
-                        search_string = "connect";
-                    };
-                    "Disconnect Warp" = {
-                        exec = "warp-cli disconnect";
-                        search_string = "disconnect";
-                    };
+[
+    {
+        name = "Clipboard";
+        type = "clipboard-execution";
+        args = {
+            capabilities = ["url" "colors.all"];
+        };
+        priority = 1;
+        home = true;
+    }
+    {
+        name = "Calculator";
+        type = "calculation";
+        args = {
+            capabilities = ["calc.math" "calc.units"];
+        };
+        priority = 1;
+    }
+    {
+        name = "App Launcher";
+        alias = "app";
+        type = "app_launcher";
+        args = {};
+        priority = 1;
+        home = true;
+    }
+    {
+        name = "Cloudflare Warp";
+        alias = "w";
+        type = "command";
+        args = {
+            commands = {
+                "Connect Warp" = {
+                    exec = "warp-cli connect";
+                    search_string = "connect";
+                };
+                "Disconnect Warp" = {
+                    exec = "warp-cli disconnect";
+                    search_string = "disconnect";
                 };
             };
-            priority = 4;
-        }
-        {
-            name = "Power Management";
-            alias = ":";
-            type = "command";
-            args = {
-                commands = {
-                    Shutdown = {
-                        icon = "system-shutdown";
-                        exec = "systemctl poweroff";
-                        search_string = "poweroff;shutdown";
-                    };
-                    Sleep = {
-                        icon = "system-suspend";
-                        exec = "systemctl suspend";
-                        search_string = "sleep;";
-                    };
-                    Lock = {
-                        icon = "system-lock-screen";
-                        exec = "hyprlock && systemctl suspend";
-                        search_string = "lock;";
-                    };
-                    Reboot = {
-                        icon = "system-reboot";
-                        exec = "systemctl reboot";
-                        search_string = "reboot";
-                    };
+        };
+        priority = 4;
+    }
+    {
+        name = "Power Management";
+        alias = ":";
+        type = "command";
+        args = {
+            commands = {
+                Shutdown = {
+                    icon = "system-shutdown";
+                    exec = "systemctl poweroff";
+                    search_string = "poweroff;shutdown";
+                };
+                Sleep = {
+                    icon = "system-suspend";
+                    exec = "systemctl suspend";
+                    search_string = "sleep;";
+                };
+                Lock = {
+                    icon = "system-lock-screen";
+                    exec = "hyprlock && systemctl suspend";
+                    search_string = "lock;";
+                };
+                Reboot = {
+                    icon = "system-reboot";
+                    exec = "systemctl reboot";
+                    search_string = "reboot";
                 };
             };
-            priority = 4;
-        }
-    ];
-}
+        };
+        priority = 4;
+    }
+]
