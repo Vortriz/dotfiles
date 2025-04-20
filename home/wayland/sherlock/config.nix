1fdc73b9be1554f08f0621ab2c0c118ba07bf725
Vortriz <vorarishi22+github@gmail.com>
2025-04-20 23:33:01 +0530

    tweak(sherlock): split

--- home/wayland/sherlock/config.nix
+++ home/wayland/sherlock/config.nix
@@ -1,20 +1,18 @@
 {
-    programs.sherlock.settings.config = {
-        appearance = {
-            width = 600;
-            height = 400;
-            icon_paths = ["~/.config/sherlock/icons/"];
-            icon_size = 22;
-            search_icon = false;
-            status_bar = false;
-        };
+    appearance = {
+        width = 600;
+        height = 400;
+        icon_paths = ["~/.config/sherlock/icons/"];
+        icon_size = 22;
+        search_icon = false;
+        status_bar = false;
+    };
 
-        behavior = {
-            caching = true;
-        };
+    behavior = {
+        caching = true;
+    };
 
-        default_apps = {
-            terminal = "kitty";
-        };
+    default_apps = {
+        terminal = "kitty";
     };
 }
--- home/wayland/sherlock/default.nix
+++ home/wayland/sherlock/default.nix
@@ -1,15 +1,17 @@
 {
-    imports = [
-        ./aliases.nix
-        ./config.nix
-        ./launchers.nix
-    ];
+    programs.sherlock = {
+        enable = true;
 
-    programs.sherlock.enable = true;
+        settings = {
+            aliases = import ./aliases.nix;
+            config = import ./config.nix;
+            launchers = import ./launchers.nix;
+        };
 
-    home.file.".config/sherlock/main.css".text = with builtins;
-        readFile (fetchurl {
-            url = "https://raw.githubusercontent.com/Skxxtz/sherlock/refs/heads/documentation/resources/main.css";
-            sha256 = "sha256-0K6KiNBq26Yvf2epkJNj9Yc/C6F3KQ6xXCrbbj0bTwM=";
-        });
+        style = with builtins;
+            readFile (fetchurl {
+                url = "https://raw.githubusercontent.com/Skxxtz/sherlock/refs/heads/documentation/resources/main.css";
+                sha256 = "sha256-0K6KiNBq26Yvf2epkJNj9Yc/C6F3KQ6xXCrbbj0bTwM=";
+            });
+    };
 }
