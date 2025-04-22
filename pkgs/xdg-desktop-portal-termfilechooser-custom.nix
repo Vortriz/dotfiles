3a6282162633646b8dcd9b795736c9f8124a75b4
Rishi Vora <vorarishi22@gmail.com>
2025-04-22 21:21:29 +0530

    tweak(termfilechooser): back to hunkyburrito's version

--- pkgs/xdg-desktop-portal-termfilechooser-custom.nix
+++ pkgs/xdg-desktop-portal-termfilechooser-custom.nix
@@ -1,6 +1,6 @@
 {
+    callPackages,
     stdenv,
-    fetchFromGitHub,
     lib,
     xdg-desktop-portal,
     ninja,
@@ -9,39 +9,34 @@
     inih,
     systemd,
     scdoc,
-}:
-stdenv.mkDerivation {
-    pname = "xdg-desktop-portal-termfilechooser";
-    version = "1.0.5-1";
+}: let
+    source = (callPackages ./sources/generated.nix {}).xdg-desktop-portal-termfilechooser;
+in
+    stdenv.mkDerivation {
+        inherit (source) src pname;
+        version = "unstable-${source.date}";
 
-    src = fetchFromGitHub {
-        owner = "Vortriz";
-        repo = "xdg-desktop-portal-termfilechooser";
-        rev = "817988fc0d41ec323f40c64ec0c320a475140a09";
-        hash = "sha256-BvN0TKXgwcbCmc7bJVWtJ0qPR1KxAsiFrva57guyxZk=";
-    };
+        nativeBuildInputs = [
+            meson
+            ninja
+            pkg-config
+            scdoc
+        ];
 
-    nativeBuildInputs = [
-        meson
-        ninja
-        pkg-config
-        scdoc
-    ];
+        buildInputs = [
+            xdg-desktop-portal
+            inih
+            systemd
+        ];
 
-    buildInputs = [
-        xdg-desktop-portal
-        inih
-        systemd
-    ];
+        mesonFlags = ["-Dsd-bus-provider=libsystemd"];
 
-    mesonFlags = ["-Dsd-bus-provider=libsystemd"];
-
-    meta = with lib; {
-        description = "xdg-desktop-portal backend for choosing files with your favorite file chooser";
-        homepage = "https://github.com/hunkyburrito/xdg-desktop-portal-termfilechooser";
-        license = licenses.mit;
-        platforms = platforms.linux;
-        maintainers = with lib.maintainers; [];
-        mainProgram = "xdg-desktop-portal-termfilechooser";
-    };
-}
+        meta = with lib; {
+            description = "xdg-desktop-portal backend for choosing files with your favorite file chooser";
+            homepage = "https://github.com/hunkyburrito/xdg-desktop-portal-termfilechooser";
+            license = licenses.mit;
+            platforms = platforms.linux;
+            maintainers = with lib.maintainers; [];
+            mainProgram = "xdg-desktop-portal-termfilechooser";
+        };
+    }
