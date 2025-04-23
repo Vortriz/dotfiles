c2678d6e72acb9757467ea83699610d463bb2fd2
Vortriz <vorarishi22+github@gmail.com>
2025-04-23 19:55:57 +0530

    fix(sherlock): deprecation

--- home/wayland/sherlock/default.nix
+++ home/wayland/sherlock/default.nix
@@ -6,12 +6,12 @@
             aliases = import ./aliases.nix;
             config = import ./config.nix;
             launchers = import ./launchers.nix;
-        };
 
-        style = with builtins;
-            readFile (fetchurl {
-                url = "https://raw.githubusercontent.com/Skxxtz/sherlock/refs/heads/documentation/resources/main.css";
-                sha256 = "sha256-0K6KiNBq26Yvf2epkJNj9Yc/C6F3KQ6xXCrbbj0bTwM=";
-            });
+            style = with builtins;
+                readFile (fetchurl {
+                    url = "https://raw.githubusercontent.com/Skxxtz/sherlock/refs/heads/documentation/resources/main.css";
+                    sha256 = "sha256-0K6KiNBq26Yvf2epkJNj9Yc/C6F3KQ6xXCrbbj0bTwM=";
+                });
+        };
     };
 }
