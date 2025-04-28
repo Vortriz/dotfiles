12ab612f459a95f466f976e9f667c7b28e6c2cc4
Vortriz <vorarishi22+github@gmail.com>
2025-04-28 10:34:15 +0530

    fix(starship): hash

--- home/terminal/shell/starship.nix
+++ home/terminal/shell/starship.nix
@@ -5,7 +5,7 @@
         settings = with builtins;
             fromTOML (readFile (fetchurl {
                 url = "https://starship.rs/presets/toml/nerd-font-symbols.toml";
-                sha256 = "1bk2bb4nfwppg0gz4yyid4prdcf5r7yis0h4x10l8blvzlzm452g";
+                sha256 = "05yvqiycb580mnym7q8lvk1wcvpq7rc4jjqb829z3s82wcb9cmbr";
             }));
     };
 }
