335549282ab86e708a8289430fc8c9ad4b5ca8ec
Vortriz <vorarishi22+github@gmail.com>
2025-04-28 10:33:19 +0530

    tweak(sherlock): snappier animation

--- home/wayland/sherlock/default.nix
+++ home/wayland/sherlock/default.nix
@@ -13,7 +13,8 @@
                 readFile (fetchurl {
                     url = "https://raw.githubusercontent.com/Skxxtz/sherlock/refs/heads/documentation/resources/main.css";
                     sha256 = "sha256-0K6KiNBq26Yvf2epkJNj9Yc/C6F3KQ6xXCrbbj0bTwM=";
-                });
+                })
+                + readFile ./extra.css;
         };
     };
 }
