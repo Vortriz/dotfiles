158016cec1d1bdeb0d58210041db31d90ee15080
Rishi Vora <vorarishi22@gmail.com>
2025-05-19 02:36:09 +0530

    fix(zed): formatters and LSPs tweak(zed): more configuration

--- home/programming/zed/default.nix
+++ home/programming/zed/default.nix
@@ -7,19 +7,33 @@
     programs.zed-editor = {
         enable = true;
 
-        userSettings = import ./settings.nix {inherit lib osConfig;};
+        userSettings = import ./settings.nix {inherit lib osConfig pkgs;};
 
         extensions = [
-            "nix"
+            # keep-sorted start
+            "fish"
+            "julia"
             "just"
             "just-ls"
             "latex"
+            "nix"
+            "toml"
             "typst"
             "vscode-icons"
+            # keep-sorted end
         ];
 
         extraPackages = with pkgs; [
+            # keep-sorted start
+            alejandra
+            nil
             nixd
+            package-version-server
+            rust-analyzer
+            taplo-lsp
+            texlab
+            tinymist
+            # keep-sorted end
         ];
     };
 
--- home/programming/zed/settings.nix
+++ home/programming/zed/settings.nix
@@ -1,19 +1,42 @@
 {
     lib,
     osConfig,
+    pkgs,
     ...
 }: let
-    inherit (osConfig.var) monospaceFontName shell;
+    inherit (osConfig.var) monospaceFontName;
+    inherit (osConfig.defaults) shell;
+
+    fontName = lib.mkForce monospaceFontName;
     fontSize = lib.mkForce 18;
 in {
-    buffer_font_family = lib.mkForce monospaceFontName;
+    # keep-sorted start block=yes
+    auto_update = false;
+    buffer_font_family = fontName;
     buffer_font_size = fontSize;
-    git = {
-        inline_blame = {
-            enabled = false;
+    cursor_shape = "bar";
+    git.inline_blame.enabled = false;
+    icon_theme = "VSCode Icons (Dark)";
+    languages = {
+        Nix = {
+            language_servers = ["nil" "nixd"];
+            formatter.external = {
+                command = "alejandra";
+                arguments = ["--quiet"];
+            };
+        };
+        Typst = {
+            language_servers = ["tinymist"];
         };
     };
-    icon_theme = "VSCode Icons (Dark)";
+    lsp = lib.mergeAttrsList (
+        map (name: {
+            ${name}.binary = {
+                path_lookup = true;
+                path = lib.getExe pkgs.${name};
+            };
+        }) ["rust-analyzer" "nixd" "nil" "tinymist"]
+    );
     minimap = {
         show = "always";
         thumb = "always";
@@ -23,15 +46,11 @@ in {
         file_icons = true;
         git_status = true;
     };
-    terminal = {
-        detect_venv = {
-            on = {
-                activate_script = shell;
-            };
-        };
-    };
-    ui_font_family = lib.mkForce monospaceFontName;
+    terminal.detect_venv.on.activate_script = "${lib.getName shell}";
+    toolbar.title = true;
+    ui_font_family = fontName;
     ui_font_size = fontSize;
     unnecessary_code_fade = 0.5;
     use_smartcase_search = true;
+    # keep-sorted end
 }
