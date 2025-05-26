2d2abf22a5f14e13daf4151050b046d3c2180d71
Vortriz <vorarishi22+github@gmail.com>
2025-05-26 19:15:42 +0530

    feat(cosmic-greeter): use

--- system/programs/niri.nix
+++ system/programs/niri.nix
@@ -4,14 +4,14 @@
     ...
 }: {
     imports = [inputs.niri.nixosModules.niri];
-
     nixpkgs.overlays = [inputs.niri.overlays.niri];
 
     programs.niri = {
         enable = true;
-
         package = pkgs.niri-unstable;
     };
 
     niri-flake.cache.enable = true;
+
+    services.displayManager.cosmic-greeter.enable = true;
 }
