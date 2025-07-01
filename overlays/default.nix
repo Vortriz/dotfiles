ed56ef1eeeb70eca3b1671d12e1aeb5b3c3a6e51
Vortriz <vorarishi22+github@gmail.com>
2025-07-01 23:23:05 +0530

    tweak(flameshot): modify overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,15 +6,7 @@ _: {
     # https://nixos.wiki/wiki/Overlays
     modifications = _final: prev: {
         # keep-sorted start block=yes newline_separated=yes
-        flameshot = prev.flameshot.overrideAttrs (_old: {
-            src = prev.pkgs.fetchFromGitHub {
-                owner = "flameshot-org";
-                repo = "flameshot";
-                rev = "63a4ab669bba83bdde878963df80a3c4e9331e21";
-                sha256 = "1axif6lfc35f10150dlq1k0l3j4ssvj02vg72lvwk99mxs41cg7q";
-            };
-            cmakeFlags = ["-DUSE_WAYLAND_GRIM=true" "-DUSE_WAYLAND_CLIPBOARD=true"];
-        });
+        flameshot = prev.flameshot.override {enableWlrSupport = true;};
 
         # https://discourse.nixos.org/t/request-for-libfprint-port-for-2808-a658/55474/32?u=vortriz
         fprintd = prev.fprintd.overrideAttrs (_old: rec {
