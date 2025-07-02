3216c5b8def660c61d3d02ec0641ef0aad7356bf
Vortriz <vorarishi22+github@gmail.com>
2025-07-03 02:09:38 +0530

    fix(sherlock): use walker temporarily

--- home/wayland/sherlock/default.nix
+++ home/wayland/sherlock/default.nix
@@ -3,6 +3,7 @@
     inputs,
     lib,
     osConfig,
+    pkgs,
     ...
 }: let
     inherit (osConfig.defaults) launcher;
@@ -25,11 +26,13 @@ in {
     };
 
     programs.niri.settings.binds = {
-        "Alt+Space" = config.niri-lib.open {app = launcher;};
+        # "Alt+Space" = config.niri-lib.open {app = launcher;};
 
-        "Mod+Period" = config.niri-lib.run {
-            cmd = "${lib.getExe launcher} --sub-menu emoji";
-            title = "Open emoji picker";
-        };
+        # "Mod+Period" = config.niri-lib.run {
+        #     cmd = "${lib.getExe launcher} --sub-menu emoji";
+        #     title = "Open emoji picker";
+        # };
+
+        "Alt+Space" = config.niri-lib.open {app = pkgs.walker;};
     };
 }
