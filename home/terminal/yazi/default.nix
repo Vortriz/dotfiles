65fc2660d7d6a5279237eab831233f466112bdee
Vortriz <vorarishi22+github@gmail.com>
2025-07-18 17:34:40 +0530

    refactor: cleanup yazi too

--- home/terminal/yazi/default.nix
+++ home/terminal/yazi/default.nix
@@ -5,7 +5,9 @@
     osConfig,
     ...
 }: let
-    inherit (osConfig.defaults) file-manager;
+    inherit (osConfig.defaults) terminal;
+
+    yazi = config.programs.yazi.package;
 in {
     imports = [
         ./keymap.nix
@@ -28,7 +30,22 @@ in {
     ];
 
     programs.niri.settings.binds."Mod+E" = config.niri-lib.open-tui {
-        app = lib.getExe file-manager;
-        app-id = lib.getName file-manager;
+        app = lib.getExe yazi;
+        app-id = lib.getName yazi;
+    };
+
+    xdg = {
+        portal = {
+            extraPortals = [pkgs.xdg-desktop-portal-termfilechooser-git];
+
+            config.niri."org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+        };
+
+        configFile."xdg-desktop-portal-termfilechooser/config".text = ''
+            [filechooser]
+            cmd=${pkgs.xdg-desktop-portal-termfilechooser-git}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+            default_dir=$HOME
+            env=TERMCMD=${lib.getName terminal}
+        '';
     };
 }
