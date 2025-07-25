c242f83d592f1f1c922564377e0acb2bbf7f1a11
Vortriz <vorarishi22+github@gmail.com>
2025-07-25 14:51:35 +0530

    tweak(yazi): disable tab hops in bunny

--- home/terminal/yazi/init.lua
+++ home/terminal/yazi/init.lua
@@ -23,7 +23,7 @@ require("bunny"):setup({
   },
   desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
   ephemeral = true, -- Enable ephemeral hops, default is true
-  tabs = true, -- Enable tab hops, default is true
+  tabs = false, -- Enable tab hops, default is true
   notify = false, -- Notify after hopping, default is false
   fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
 })
