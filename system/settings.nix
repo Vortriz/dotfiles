123cda7bcbdb2079600cb44523756cc165e5343b
Vortriz <vorarishi22+github@gmail.com>
2025-08-07 12:20:20 +0530

    tweak: turn on proxy

--- system/settings.nix
+++ system/settings.nix
@@ -77,10 +77,10 @@ in {
         # Enables wireless support via wpa_supplicant.
         # wireless.enable = true;
 
-        # proxy = {
-        # default = "http://172.16.2.250:3128";
-        # noProxy = "127.0.0.1,localhost,internal.domain";
-        # };
+        proxy = {
+            default = "http://172.16.2.250:3128";
+            noProxy = "127.0.0.1,localhost,internal.domain";
+        };
 
         networkmanager.enable = true;
         firewall.enable = true;
