85fba1f1532b05429bbdb4c6d95cc9eb56870065
Vortriz <vorarishi22+github@gmail.com>
2025-08-18 11:52:24 +0530

    chore(nix-search-tv): update nixpkgs.sh hash

--- home/terminal/nix-search-tv/default.nix
+++ home/terminal/nix-search-tv/default.nix
@@ -7,7 +7,7 @@
     ns =
         builtins.fetchurl {
             url = "https://raw.githubusercontent.com/3timeslazy/nix-search-tv/refs/heads/main/nixpkgs.sh";
-            sha256 = "sha256:0k7vkd83zgbwr3fgkf4nszwaq3ab9344w4nq9krj75fpla1vawxz";
+            sha256 = "sha256:1z5jgi27yrisy3rwrba01kj2chpq68zdr5g5aalh5y5xd0rv0j3c";
         }
         |> builtins.readFile
         |> pkgs.writeShellScriptBin "ns";
