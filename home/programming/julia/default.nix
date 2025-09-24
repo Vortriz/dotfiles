262f5876cbeccc1dd5b3cc057ef9165bb435ab93
Vortriz <vorarishi22+github@gmail.com>
2025-09-24 13:54:13 +0530

    remove julia startup.jl

--- home/programming/julia/default.nix
+++
@@ -1,9 +0,0 @@
-{
-    xdg.dataFile."julia/config/startup.jl".text = ''
-        using Pkg
-
-        if isfile("Project.toml") && isfile("Manifest.toml")
-            Pkg.activate(".")
-        end
-    '';
-}
