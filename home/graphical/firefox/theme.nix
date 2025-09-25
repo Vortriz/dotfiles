999bd3616f41a6b2d6caee7b35988f928b2d9557
Vortriz <vorarishi22+github@gmail.com>
2025-09-25 12:51:59 +0530

    fix(firefox): shyfox

--- home/graphical/firefox/theme.nix
+++ home/graphical/firefox/theme.nix
@@ -3,9 +3,13 @@
 
     shyfox = pkgs.fetchzip {
         url = "https://github.com/Vortriz/ShyFox/archive/refs/heads/main.zip";
-        sha256 = "sha256-6sD2xcO/M7Exc15zJPJk+qSw/qvaWt7YaFRZ3ZeHuCU=";
+        sha256 = "sha256-5GsZ9SORT+qaYhDY7EzI0DULzR68qzDJvW0mDKR5PSQ=";
         stripRoot = true;
     };
+    # For testing purposes
+    # shyfox = builtins.path {
+    #     path = /mnt/HOUSE/dev/ShyFox;
+    # };
 in {
     programs.firefox.profiles.default.extraConfig = builtins.readFile "${shyfox}/user.js";
 
