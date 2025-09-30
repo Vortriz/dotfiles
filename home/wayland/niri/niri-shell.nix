4f2a9e6b81b3c3d876e1c524b37d2ebc0d565868
Vortriz <vorarishi22+github@gmail.com>
2025-09-30 22:11:35 +0530

    chore(niri-shell): update

--- home/wayland/niri/niri-shell.nix
+++ home/wayland/niri/niri-shell.nix
@@ -3,13 +3,18 @@
     pkgs,
     ...
 }: {
-    imports = [inputs.niri-shell.homeModules.dankMaterialShell];
+    imports = [
+        inputs.niri-shell.homeModules.dankMaterialShell.default
+        inputs.niri-shell.homeModules.dankMaterialShell.niri
+    ];
 
     programs.dankMaterialShell = {
         enable = true;
         quickshell.package = inputs.niri-shell.packages.${pkgs.system}.quickshell;
 
-        enableKeybinds = true;
-        enableSpawn = true;
+        niri = {
+            enableKeybinds = true;
+            enableSpawn = true;
+        };
     };
 }
