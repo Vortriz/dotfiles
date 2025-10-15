e2173630a22eef4fb5bb13f434eaab93b1f1c0da
Vortriz <vorarishi22+github@gmail.com>
2025-10-15 15:37:50 +0530

    chore: cleanup

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,10 +4,10 @@ pkgs: let
     inherit (pkgs) callPackage;
     callPackage' = pkg: pkgs.callPackage pkg {sources = pkgs.callPackage ./sources/generated.nix {};};
 in {
+    # Fonts
     HelveticaNeueCyr = callPackage ./fonts/HelveticaNeueCyr.nix {};
     SFMono = callPackage ./fonts/SFMono.nix {};
 
-    swww-git = callPackage ./swww/package.nix {}; # remove when swww 0.10.4 is in nixpkgs
-    xdg-desktop-portal-termfilechooser-git = callPackage' ./xdg-desktop-portal-termfilechooser-git/package.nix;
     dunefetch-git = callPackage' ./dunefetch-git/package.nix;
+    xdg-desktop-portal-termfilechooser-git = callPackage' ./xdg-desktop-portal-termfilechooser-git/package.nix;
 }
