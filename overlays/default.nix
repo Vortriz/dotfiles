dbdbd14a97a4ad7eb7418d173c42e6c4cb726a50
Vortriz <vorarishi22+github@gmail.com>
2025-10-16 01:27:32 +0530

    fix(fprintd): change source of driver

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,7 +7,7 @@ _: {
     modifications = _final: prev: {
         # keep-sorted start block=yes newline_separated=yes
         # https://discourse.nixos.org/t/request-for-libfprint-port-for-2808-a658/55474/32?u=vortriz
-        fprintd = prev.fprintd.overrideAttrs (_old: rec {
+        fprintd = prev.fprintd.overrideAttrs (_: rec {
             version = "1.94.4";
             src = prev.pkgs.fetchFromGitLab {
                 domain = "gitlab.freedesktop.org";
@@ -15,6 +15,14 @@ _: {
                 repo = "fprintd";
                 rev = "refs/tags/v${version}}";
                 hash = "sha256-B2g2d29jSER30OUqCkdk3+Hv5T3DA4SUKoyiqHb8FeU=";
+            };
+        });
+
+        libfprint-focaltech-2808-a658 = prev.libfprint-focaltech-2808-a658.overrideAttrs (_: rec {
+            version = "1.94.4";
+            src = prev.pkgs.fetchurl {
+                url = "https://github.com/Varrkan82/RTS5811-FT9366-fingerprint-linux-driver-with-VID-2808-and-PID-a658/raw/b040ccd953c27e26c1285c456b4264e70b36bc3f/libfprint-2-2-${version}+tod1-FT9366_20240627.x86_64.rpm";
+                hash = "sha256-MRWHwBievAfTfQqjs1WGKBnht9cIDj9aYiT3YJ0/CUM=";
             };
         });
         # keep-sorted end
