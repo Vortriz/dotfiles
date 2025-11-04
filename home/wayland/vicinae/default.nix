bca5ae806be4572cc6541acc5298c4ce3b503015
Vortriz <vorarishi22+github@gmail.com>
2025-11-04 16:02:32 +0530

    fix(vicinae): dont manage settings with HM

--- home/wayland/vicinae/default.nix
+++ home/wayland/vicinae/default.nix
@@ -9,23 +9,6 @@
     services.vicinae = {
         enable = true;
         package = inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system}.default;
-
-        settings = {
-            closeOnFocusLoss = false;
-            faviconService = "google";
-            font = {
-                normal = "Maple Mono Normal NF";
-                size = 10;
-            };
-            popToRootOnClose = true;
-            rootSearch.searchFiles = false;
-            theme.name = "vicinae-dark";
-            window = {
-                csd = true;
-                opacity = 1;
-                rounding = 10;
-            };
-        };
     };
 
     programs.niri.settings.binds = {
