da597630e13373eb2bf718d1c52e66a29bb5ee0d
Vortriz <vorarishi22+github@gmail.com>
2025-11-09 00:43:44 +0530

    feat(vicinae): add goldfish package for fuzzy file search plugin

--- home/wayland/vicinae/default.nix
+++ home/wayland/vicinae/default.nix
@@ -11,6 +11,10 @@
         package = inputs.vicinae.packages.${pkgs.stdenv.hostPlatform.system}.default;
     };
 
+    home.packages = with pkgs; [
+        nur.repos.Vortriz.goldfish
+    ];
+
     programs.niri.settings.binds = {
         "Alt+Space" = config.niri-lib.open {
             app = config.services.vicinae.package;
