6559b00cb1e29180abf284f53bed9a00235b1cee
Vortriz <vorarishi22+github@gmail.com>
2025-11-11 12:10:30 +0530

    chore(vscode): cleanup

--- home/programming/vscode/extensions.nix
+++ home/programming/vscode/extensions.nix
@@ -3,34 +3,28 @@
         # language packs et al
 
         # keep-sorted start
+        charliermarsh.ruff
         james-yu.latex-workshop
         jnoortheen.nix-ide
-        julialang.language-julia
-        mikestead.dotenv
-        ms-python.isort
         ms-python.python
-        ms-python.vscode-pylance
         ms-toolsai.jupyter
         ms-toolsai.jupyter-renderers
         ms-toolsai.vscode-jupyter-cell-tags
         myriad-dreamin.tinymist
         nefrob.vscode-just-syntax
-        rust-lang.rust-analyzer
         shd101wyy.markdown-preview-enhanced
-        tamasfe.even-better-toml
         yzhang.markdown-all-in-one
         # keep-sorted end
 
         # misc
 
         # keep-sorted start
+        Google.gemini-cli-vscode-ide-companion
         donjayamanne.githistory
         github.copilot
         github.copilot-chat
         jock.svg
         mkhl.direnv
-        ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-containers
         ms-vsliveshare.vsliveshare
         ritwickdey.liveserver
         rubymaniac.vscode-paste-and-indent
