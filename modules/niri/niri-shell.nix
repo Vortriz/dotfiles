943eeb7fc5587f90565fa58854b2cdd1adf1f295
Vortriz <vorarishi22+github@gmail.com>
2025-12-25 14:45:20 +0530

    feat(gdm): go back to gdm

--- modules/niri/niri-shell.nix
+++ modules/niri/niri-shell.nix
@@ -1,45 +1,14 @@
 {inputs, ...}: {
     unify = {
         nixos = {
-            homeConfig,
-            pkgs,
-            ...
-        }: {
-            imports = [inputs.niri-shell.nixosModules.greeter];
-
             nixpkgs.overlays = [
                 inputs.quickshell.overlays.default
             ];
 
             # to use dms-greet
-            systemd.user.services.niri-flake-polkit.enable = false;
+            systemd.user.services.niri-flake-polkit.enable = true;
 
-            programs.dank-material-shell.greeter = {
-                enable = true;
-                compositor.name = "niri";
-
-                configFiles = [
-                    "/etc/dms-greeter/settings.json"
-                    "${homeConfig.xdg.stateHome}/DankMaterialShell/session.json"
-                    "${homeConfig.xdg.cacheHome}/DankMaterialShell/dms-colors.json"
-                ];
-
-                logs.save = true;
-            };
-
-            environment.etc."dms-greeter/settings.json".source = pkgs.writers.writeJSON "settings.json" {
-                weatherEnabled = false;
-                currentThemeName = "blue";
-                matugenScheme = "scheme-tonal-spot";
-                iconTheme = "System Default";
-                fontFamily = homeConfig.stylix.fonts.monospace.name;
-                fontWeight = 400;
-                fontScale = 1;
-                cornerRadius = 12;
-                widgetBackgroundColor = "sch";
-                surfaceBase = "s";
-                animationSpeed = 2;
-            };
+            services.displayManager.gdm.enable = true;
         };
 
         home = {pkgs, ...}: {
