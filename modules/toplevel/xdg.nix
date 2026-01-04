bb2ed2f4cc4a257de6290356751009b9541a9d33
Vortriz <vorarishi22+github@gmail.com>
2026-01-04 16:48:35 +0530

    fix(xdg): annoying warning

--- modules/toplevel/xdg.nix
+++ modules/toplevel/xdg.nix
@@ -1,18 +1,24 @@
 {
-    unify.home = {hostConfig, ...}: let
-        inherit (hostConfig.dirs) downloads;
-    in {
-        xdg = {
-            enable = true;
-            # For setting default applications
-            mimeApps.enable = true;
+    unify = {
+        nixos = {
+            environment.pathsToLink = ["/share/xdg-desktop-portal" "/share/applications"];
+        };
 
-            userDirs = {
+        home = {hostConfig, ...}: let
+            inherit (hostConfig.dirs) downloads;
+        in {
+            xdg = {
                 enable = true;
+                # For setting default applications
+                mimeApps.enable = true;
 
-                download = downloads;
-                pictures = "${downloads}/media";
-                videos = "${downloads}/media";
+                userDirs = {
+                    enable = true;
+
+                    download = downloads;
+                    pictures = "${downloads}/media";
+                    videos = "${downloads}/media";
+                };
             };
         };
     };
