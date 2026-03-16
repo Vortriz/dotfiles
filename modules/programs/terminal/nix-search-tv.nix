76678cc1ef8e1fbfa7efab8793467e0c863755e6
Vortriz <vorarishi22+github@gmail.com>
2026-03-16 17:38:48 +0530

    tweak(nix-search-tv): index noogle too

--- modules/programs/terminal/nix-search-tv.nix
+++ modules/programs/terminal/nix-search-tv.nix
@@ -14,6 +14,10 @@
             ns
         ];
 
+        xdg.configFile."nix-search-tv/config.json".text = lib.toJSON {
+            indexes = ["nixpkgs" "nixos" "home-manager" "nur" "noogle"];
+        };
+
         wayland.windowManager.niri.settings.binds."Mod+P" = {
             spawn-sh = "xdg-terminal-exec --app-id=nix-search-tv ${lib.getExe ns}";
             _props.hotkey-overlay-title = "Open nix-search-tv";
