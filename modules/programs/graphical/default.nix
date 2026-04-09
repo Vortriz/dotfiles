7d9f64ef045a94094258661ab82fbe3b1984dd1e
Vortriz <vorarishi22+github@gmail.com>
2026-04-09 18:48:56 +0530

    fix(obsidian): electron incompatibility

--- modules/programs/graphical/default.nix
+++ modules/programs/graphical/default.nix
@@ -3,11 +3,11 @@
         home.packages = with pkgs; [
             # keep-sorted start
             (bottles.override {removeWarningPopup = true;})
+            (obsidian.override {electron = pkgs.electron_39;})
             anydesk
             gnome-calculator
             gnome-characters
             libreoffice
-            obsidian
             onlyoffice-desktopeditors
             pavucontrol
             pdfpc
